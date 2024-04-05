ef493f99c77eb46985b54bec9cb3a6b5210f6d36
Nathan Smith <nathanielvs@gmail.com>
2024-04-05 15:21:54 -0600

    Add Feed and Post components. Add basic blog content rendering to main components. Add new styles.

--- src/App.js
+++ src/App.js
@@ -6,38 +6,63 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Router, Route, Switch, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: "My First Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 2,
+            title: "My 2nd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 3,
+            title: "My 3rd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 4,
+            title: "My Fourth Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+    ]);
+    const [search, setSearch] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const history = useHistory();
+
+    const handleDelete = (id) => {
+        const postsList = posts.filter((post) => post.id !== id);
+        setPosts(postsList);
+        // Return to homepage
+        history.push("/");
+    };
+
     return (
         <div className="App">
-            <Header />
-            <Nav />
+            <Header title="The Wanderer Blog" />
+            <Nav search={search} setSearch={setSearch} />
             <Switch>
-                <Route
-                    exact
-                    path="/"
-                >
-                    <Home />
+                <Route exact path="/">
+                    <Home posts={posts} />
                 </Route>
-                <Route
-                    exact
-                    path="/post"
-                >
+                <Route exact path="/post">
                     <NewPost />
                 </Route>
                 <Route path="/post/:id">
-                    <PostPage />
+                    <PostPage posts={posts} handleDelete={handleDelete} />
                 </Route>
-                <Route
-                    path="/about"
-                    component={About}
-                />
-                <Route
-                    path="*"
-                    component={Missing}
-                />
+                <Route path="/about" component={About} />
+                <Route path="*" component={Missing} />
             </Switch>
             <Footer />
         </div>
