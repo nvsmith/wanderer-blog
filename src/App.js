5741cd55533f9f86f0df27039c310044a43715cd
Nathan Smith <nathanielvs@gmail.com>
2024-04-05 21:22:13 -0600

    Add state for post body and title. Pass state and submit props to NewPost component and render the form to create a new post.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,11 @@ function App() {
     ]);
     const [search, setSearch] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+    const [postTitle, setPostTitle] = useState("");
+    const [postBody, setPostBody] = useState("");
     const history = useHistory();
+
+    const handleSubmit = () => {};
 
     const handleDelete = (id) => {
         const postsList = posts.filter((post) => post.id !== id);
@@ -56,7 +60,13 @@ function App() {
                     <Home posts={posts} />
                 </Route>
                 <Route exact path="/post">
-                    <NewPost />
+                    <NewPost
+                        handleSubmit={handleSubmit}
+                        postTitle={postTitle}
+                        setPostTitle={setPostTitle}
+                        postBody={postBody}
+                        setPostBody={setPostBody}
+                    />
                 </Route>
                 <Route path="/post/:id">
                     <PostPage posts={posts} handleDelete={handleDelete} />
