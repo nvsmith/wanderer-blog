b6a4fb5fdab17847dead88a1dd726d2d19f66fcb
Nathan Smith <nathanielvs@gmail.com>
2024-04-08 12:19:18 -0600

    Set up Axios useEffect logic to get data from URL

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Missing from "./Missing";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
+import api from "./api/posts";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -15,6 +16,25 @@ function App() {
     const [postTitle, setPostTitle] = useState("");
     const [postBody, setPostBody] = useState("");
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await api.get("/posts");
+                setPosts(response.data);
+            } catch (err) {
+                if (err.response) {
+                    // Not in HTTP 200 response range
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        };
+        fetchPosts();
+    }, []);
 
     useEffect(() => {
         const filteredResults = posts.filter(
